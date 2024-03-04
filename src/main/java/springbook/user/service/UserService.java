2ddbae7bf0c26efd8d4cbb1fdb113973decdee95
gonnichiwa <keepthefaith00@gmail.com>
2024-03-04 15:59:31 +0900

    userservice.upgradeLevel() 읽기쉽게1

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -18,22 +18,27 @@ public class UserService {
     // 레벨 변경은 일정한 주기로 수행. 변경작업전에는 조건 충족하더라도 레벨 변경 없음.
     public void upgradeLevels(){
         List<User> users = userDao.getAll2();
-//        for(User user : users){
-//            Boolean changed = null;
-//            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-//                user.setLevel(Level.SILVER);
-//                changed = true;
-//            }
-//            else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30){
-//                user.setLevel(Level.GOLD);
-//                changed = true;
-//            }
-//            else if (user.getLevel() == Level.GOLD) { changed = false; }
-//            else { changed = false;}
-//            if(changed) {
-//                userDao.update(user);
-//            }
-//        }
+        for(User user : users){
+            if(canUpgradeLevel(user)){
+                upgradeLevel(user);
+            }
+        }
+    }
+
+    private void upgradeLevel(User user) {
+        if(user.getLevel() == Level.BASIC) user.setLevel(Level.SILVER);
+        else if(user.getLevel() == Level.SILVER) user.setLevel(Level.GOLD);
+        userDao.update(user);
+    }
+
+    private boolean canUpgradeLevel(User user) {
+        Level currentLev = user.getLevel();
+        switch (currentLev){
+            case BASIC: return (user.getLogin() >= 50);
+            case SILVER: return (user.getRecommend() >= 30);
+            case GOLD : return false;
+            default: throw new IllegalArgumentException("Unknown Level: " + currentLev);
+        }
     }
 
     public void add(User user) {
