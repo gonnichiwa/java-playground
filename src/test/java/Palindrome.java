e2832a3153acfa4bbb34bca6b7ed82d581052ce5
gonnichiwa <keepthefaith00@gmail.com>
2024-04-19 09:33:47 +0900

    회문test

---
+++ src/test/java/Palindrome.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class Palindrome {
+
+    @Test
+    public void 회문(){
+        String palindrome = "소주만병만주소";
+        String notPalindrome = "소주만병좀주소";
+
+        assertThat(isPalindrome(palindrome, 0, palindrome.length()-1), is(true) );
+        assertThat(isPalindrome(notPalindrome, 0, palindrome.length() - 1), is(false) );
+    }
+
+    private Boolean isPalindrome(String str, int start, int end) {
+        if(str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+        if(end - start <= 1){
+            return true;
+        }
+        return isPalindrome(str, start+1, end-1);
+    }
+}
