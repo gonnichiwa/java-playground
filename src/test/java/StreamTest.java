7350af45c968ff209e5a60fb62c9b8021fe85dd9
gonnichiwa <keepthefaith00@gmail.com>
2024-07-17 01:54:18 +0900

    update problem solved

--- src/test/java/StreamTest.java
+++ src/test/java/StreamTest.java
@@ -571,8 +571,114 @@ public class StreamTest {
     }
 
     @Test
+    public void maxNum(){
+        int[] numbers = new int[7]; // {0,0,1,1,2,0,0}
+        // param (4,4,3,2)
+        numbers[4] += 1;
+        numbers[4] += 1;
+        numbers[3] += 1;
+        numbers[2] += 1;
+        int maxnum = Arrays.stream(numbers).max().getAsInt();
+        boolean contains = Arrays.stream(numbers).anyMatch(num -> num == 1);
+        System.out.println(maxnum);
+        System.out.println(contains);
+    }
+
+    @Test
+    public void solution_181916() {
+        /*
+        * https://school.programmers.co.kr/learn/courses/30/lessons/181916
+        * 문제 설명
+        1부터 6까지 숫자가 적힌 주사위가 네 개 있습니다. 네 주사위를 굴렸을 때 나온 숫자에 따라 다음과 같은 점수를 얻습니다.
+
+        네 주사위에서 나온 숫자가 모두 p로 같다면 1111 × p점을 얻습니다.
+        세 주사위에서 나온 숫자가 p로 같고 나머지 다른 주사위에서 나온 숫자가 q(p ≠ q)라면 (10 × p + q)2 점을 얻습니다.
+        주사위가 두 개씩 같은 값이 나오고, 나온 숫자를 각각 p, q(p ≠ q)라고 한다면 (p + q) × |p - q|점을 얻습니다.
+        어느 두 주사위에서 나온 숫자가 p로 같고 나머지 두 주사위에서 나온 숫자가 각각 p와 다른 q, r(q ≠ r)이라면 q × r점을 얻습니다.
+        네 주사위에 적힌 숫자가 모두 다르다면 나온 숫자 중 가장 작은 숫자 만큼의 점수를 얻습니다.
+        네 주사위를 굴렸을 때 나온 숫자가 정수 매개변수 a, b, c, d로 주어질 때, 얻는 점수를 return 하는 solution 함수를 작성해 주세요.
+
+        제한사항
+        a, b, c, d는 1 이상 6 이하의 정수입니다.
+        입출력 예
+        a	b	c	d	result
+        2	2	2	2	2222
+        4	1	4	4	1681
+        6	3	3	6	27
+        2	5	2	6	30
+        6	4	2	5	2
+        입출력 예 설명
+        입출력 예 #1
+
+        예제 1번에서 네 주사위 숫자가 모두 2로 같으므로 1111 × 2 = 2222점을 얻습니다. 따라서 2222를 return 합니다.
+        입출력 예 #2
+
+        예제 2번에서 세 주사위에서 나온 숫자가 4로 같고 나머지 다른 주사위에서 나온 숫자가 1이므로 (10 × 4 + 1)2 = 412 = 1681점을 얻습니다. 따라서 1681을 return 합니다.
+        입출력 예 #3
+
+        예제 3번에서 a, d는 6으로, b, c는 3으로 각각 같으므로 (6 + 3) × |6 - 3| = 9 × 3 = 27점을 얻습니다. 따라서 27을 return 합니다.
+        입출력 예 #4
+
+        예제 4번에서 두 주사위에서 2가 나오고 나머지 다른 두 주사위에서 각각 5, 6이 나왔으므로 5 × 6 = 30점을 얻습니다. 따라서 30을 return 합니다.
+        입출력 예 #5
+
+        예제 5번에서 네 주사위 숫자가 모두 다르고 나온 숫자 중 가장 작은 숫자가 2이므로 2점을 얻습니다. 따라서 2를 return 합니다.
+        * */
+        assertThat(solution181916(2,2,2,2), is(2222));
+        assertThat(solution181916(4,1,4,4), is(1681));
+        assertThat(solution181916(6,3,3,6), is(27));
+        assertThat(solution181916(2,5,2,6), is(30));
+        assertThat(solution181916(6,4,2,5), is(2));
+    }
+
+    public int solution181916(int a, int b, int c, int d) {
+        int[] counts = new int[7];
+        counts[a] += 1;
+        counts[b] += 1;
+        counts[c] += 1;
+        counts[d] += 1;
+        int maxNum = Arrays.stream(counts).max().getAsInt();
+        int answer = 0;
+        if(maxNum == 4) { // 같은 주사위 숫자 4개일 때
+            return 1111 * a;
+        } else if (maxNum == 3) { // 같은 숫자 3개 일 때
+            int p=0, q=0;
+            for(int i = 0; i < counts.length; i++) {
+                if(counts[i] == 3) { p = i; }
+                if(counts[i] == 1) { q = i; }
+            }
+            return (int) Math.pow(10*p + q, 2);
+        } else if (maxNum == 2) { // 같은 숫자 2개일 때
+            int p = 0, q = 0;
+            boolean twoOneOne = Arrays.stream(counts).anyMatch(num -> num == 1); // 같은 숫자 2개, 나머지 2개 다른경우
+            if(twoOneOne) {
+                answer = 1;
+                for(int i = 0; i < counts.length; i++) {
+                    if(counts[i] == 1) answer *= i;
+                }
+                return answer;
+            } else {
+                // 같은 숫자 2개씩 나온 경우
+                for(int i = 0; i < counts.length; i++){
+                    if(counts[i] == 2) {
+                        if(p == 0) {
+                            p = i;
+                        } else {
+                            q = i;
+                        }
+                    }
+                }
+                return (p + q) * Math.abs(p - q);
+            }
+        } else {
+            // 각각 다 다른 숫자일 경우
+            return Arrays.stream(new int[]{a,b,c,d}).min().getAsInt();
+        }
+    }
+
+    @Test
     public void page(){
-        String[] printPages = getPages(122,126);
+        String[] printPages = getPages(179,191);
         for(String p : printPages) {
             System.out.println(p);
         }
